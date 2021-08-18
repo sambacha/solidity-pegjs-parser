87b594ad21e1221b5e3574ac8f69b5751a9ce4be
sam bacha <sam@manifoldfinance.com>
2021-08-18 15:52:09 -0700

    feat(refactor): migrate to peggy and various improvements
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

---
+++ test/example_incomplete.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.5.0 <0.8.0;
+
+contract Test {
+
+    address public owner;
+    address public _owner;
+    address public _awner;
+    address public _cwner;
+    address public awner;
+
+    constructor() {
+        ow
+    }
+
+    function test(int number) returns (int name) {
+        return number   
+    }
+
+    function test2(int number) returns (int name) {
+        emit something
+        emit something(1, 2);
+    }
+
+    function test3(int number) returns (int name) {
+        number.t.x
+        number.t.x = 10;
+    }
+
+    function test4(int number) returns (int name) {
+        number = test;
+        number = test
+
+    }
+
+    function test5(int number) returns (int name) {
+        number = new test(number);
+        // number = new 
+
+    }
+    
+}
