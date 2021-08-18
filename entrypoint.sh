7d3097d4994b002e7620a3beab8d66d84b79f14f
sam bacha <sam@manifoldfinance.com>
2021-08-18 16:41:17 -0700

    refactor(finalize): cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -o errexit
+export NODE_ENV='production'
+export SOURCE_COMMIT="${SOURCE_COMMIT:-$(git rev-parse HEAD)}"
 
 DIR="node_modules/.bin"
 if [ -d "$DIR" ]; then
-  ### Take action if $DIR exists ###
   echo "Building Solidity Parser..."
-  ./node_modules/.bin/peggy -o ./build/parser.js ./lib/solidity.pegjs
-  ./node_modules/.bin/peggy -o  ./build/imports_parser.js ./lib/imports.pegjs
+  echo "Parser SOURCE_COMMIT"
+  echo "SOURCE_COMMIT: $SOURCE_COMMIT"
+  sleep 1
+  npx peggy --cache -o ./build/parser.js ./solidity.pegjs
+  npx peggy -o ./build/imports_parser.js ./imports.pegjs
   echo "Geneated parser successfully"
+  exit 0
 else
   echo "Error: ${DIR} not found. Try installing with npm and not yarn"
   exit 1
